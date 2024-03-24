b35564c3ac1d2e1d9d02100e7bb6fde90bf34256
Ryan Parman <ryan@ryanparman.com>
2024-03-24 15:12:04 -0600

    ci: Add the initial GitHub Actions workflows and testing harness.

--- tests/standard/aws_resource_tags_test.go
+++ tests/standard/aws_resource_tags_test.go
@@ -27,85 +27,71 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	errTerraformPath     = "failed to set TF_ACC_TERRAFORM_PATH"
-	errProviderNamespace = "failed to set TF_ACC_PROVIDER_NAMESPACE"
-	errProviderHost      = "failed to set TF_ACC_PROVIDER_HOST"
-)
-
 var (
 	err error
 
-	tmpDir        = "./"
-	origPath      = os.Getenv("TF_ACC_TERRAFORM_PATH")
-	origNamespace = os.Getenv("TF_ACC_PROVIDER_NAMESPACE")
-	origHostname  = os.Getenv("TF_ACC_PROVIDER_HOST")
-
-	// Both must be installed first.
-	binaries = []string{
-		"terraform",
-		"tofu",
-	}
+	tmpDir = "./"
+	binary = os.Getenv("TF_PATH")
 )
 
 func TestAwsResourceTags(t *testing.T) {
 	// https://golang.org/pkg/testing/#T.Parallel
 	t.Parallel()
 
-	for i := range binaries {
-		binary := binaries[i]
-
-		if _, err = os.Stat(which.Which(binary)); os.IsNotExist(err) {
-			t.Fatalf("Binary %s must be installed first", binary)
-		}
-
-		// https://pkg.go.dev/github.com/gruntwork-io/terratest/modules/terraform#Options
-		terraformOptions := &terraform.Options{
-			// The path to Terraform.
-			TerraformBinary: which.Which(binary),
-
-			// The path to where our Terraform code is located
-			TerraformDir: tmpDir,
-
-			// Disable colors in Terraform commands so its easier to parse stdout/stderr
-			NoColor: true,
-
-			// terraform init -upgrade
-			Upgrade: true,
-
-			// terraform init -reconfigure
-			Reconfigure: true,
-
-			// Set the maximum number of parallelism; equivalent to `nproc`.
-			Parallelism: runtime.NumCPU(),
-		}
-
-		defer test_structure.RunTestStage(t, "destroy", func() {
-			terraformOptions := test_structure.LoadTerraformOptions(t, tmpDir)
-			terraform.Destroy(t, terraformOptions)
-		})
-
-		test_structure.RunTestStage(t, "tfapply", func() {
-			terraform.InitAndApply(t, terraformOptions)
-			test_structure.SaveTerraformOptions(t, tmpDir, terraformOptions)
-		})
-
-		test_structure.RunTestStage(t, "outputs", func() {
-			terraformOptions := test_structure.LoadTerraformOptions(t, tmpDir)
-			instanceTags := terraform.OutputMap(t, terraformOptions, "common_tags")
-			randomId := terraform.Output(t, terraformOptions, "random_id")
-
-			expectedTags := map[string]string{
-				"app":        fmt.Sprintf("sample-app-%s", randomId),
-				"env":        "sandbox",
-				"managed_by": "opentofu",
-			}
-
-			for tag, expectedValue := range expectedTags {
-				actualValue, doesContain := instanceTags[tag]
-				assert.True(t, doesContain)
-				assert.Equal(t, expectedValue, actualValue)
-			}
-		})
+	if binary == "" {
+		binary = "terraform"
 	}
+
+	if _, err = os.Stat(which.Which(binary)); os.IsNotExist(err) {
+		t.Fatalf("Binary %s must be installed first", binary)
+	}
+
+	// https://pkg.go.dev/github.com/gruntwork-io/terratest/modules/terraform#Options
+	terraformOptions := &terraform.Options{
+		// The path to Terraform.
+		TerraformBinary: which.Which(binary),
+
+		// The path to where our Terraform code is located
+		TerraformDir: tmpDir,
+
+		// Disable colors in Terraform commands so its easier to parse stdout/stderr
+		NoColor: true,
+
+		// terraform init -upgrade
+		Upgrade: true,
+
+		// terraform init -reconfigure
+		Reconfigure: true,
+
+		// Set the maximum number of parallelism; equivalent to `nproc`.
+		Parallelism: runtime.NumCPU(),
+	}
+
+	defer test_structure.RunTestStage(t, "destroy", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, tmpDir)
+		terraform.Destroy(t, terraformOptions)
+	})
+
+	test_structure.RunTestStage(t, "tfapply", func() {
+		terraform.InitAndApply(t, terraformOptions)
+		test_structure.SaveTerraformOptions(t, tmpDir, terraformOptions)
+	})
+
+	test_structure.RunTestStage(t, "outputs", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, tmpDir)
+		instanceTags := terraform.OutputMap(t, terraformOptions, "common_tags")
+		randomId := terraform.Output(t, terraformOptions, "random_id")
+
+		expectedTags := map[string]string{
+			"app":        fmt.Sprintf("sample-app-%s", randomId),
+			"env":        "sandbox",
+			"managed_by": "opentofu",
+		}
+
+		for tag, expectedValue := range expectedTags {
+			actualValue, doesContain := instanceTags[tag]
+			assert.True(t, doesContain)
+			assert.Equal(t, expectedValue, actualValue)
+		}
+	})
 }

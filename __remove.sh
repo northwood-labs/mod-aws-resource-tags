24e8dac8b2ca76b14d0e11a2645cb63a5f57cd03
Ryan Parman <ryan@ryanparman.com>
2024-03-25 15:16:01 -0600

    sync: Sync shared standardized files.

---
+++ __remove.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -Rfv \
+    "${PWD}/.markdownlintrc" \
+    "${PWD}/.markdownlint.base.json" \
+    "${PWD}/.markdownlint.json" \
+    ;
--- versions.tf
+++ versions.tf
@@ -1,12 +1,16 @@
 # https://opentofu.org/docs/language/settings/
 terraform {
+  # goplicate-start:version
   required_version = "~> 1.6"
+  # goplicate-end:version
 
   required_providers {
+    # goplicate-start:aws
     # https://github.com/hashicorp/terraform-provider-aws/releases/latest
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    # goplicate-end:aws
   }
 }

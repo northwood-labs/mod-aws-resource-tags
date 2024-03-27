aace66c2aa5ae459ad4f0f35be05da5dea1c67b3
Ryan Parman <ryan@ryanparman.com>
2024-03-26 19:24:43 -0600

    fix: Updated the base Terraform/OpenTofu version to ~> 1.6.

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 # https://opentofu.org/docs/language/settings/
 terraform {
   # goplicate-start:version
-  required_version = "~> 1.0"
+  required_version = "~> 1.6"
   # goplicate-end:version
 
   required_providers {
@@ -9,7 +9,7 @@ terraform {
     # https://github.com/hashicorp/terraform-provider-aws/releases/latest
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.42"
     }
     # goplicate-end:aws
   }
